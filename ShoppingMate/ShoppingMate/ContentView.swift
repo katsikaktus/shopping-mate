e48ca49ff1a9dec1221686145a824fe311152616
Giota <panagiota.papadopoulou@protonmail.com>
2024-01-20 18:06:25 +0100

    initial project

---
+++ ShoppingMate/ShoppingMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingMate
+//
+//  Created by Giota on 2024-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingMate/ShoppingMate/ShoppingMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingMateApp.swift
+//  ShoppingMate
+//
+//  Created by Giota on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
